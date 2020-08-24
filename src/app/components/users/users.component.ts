7ebeef046129e03d86bc56aa7f25edf0d358a6da
devravindersingh <ravindersngh402@outlook.com>
2020-08-24 20:40:44 +0530

    Events And Forms
    learned forms, events

--- src/app/components/users/users.component.ts
+++ src/app/components/users/users.component.ts
@@ -7,13 +7,22 @@ import { User } from 'src/app/models/User';
   styleUrls: ['./users.component.scss']
 })
 export class UsersComponent implements OnInit {
-
+  user : User = {
+    firstName : '',
+    lastName : '',
+    age : null,
+    address : {
+      street : '',
+      city : '',
+      state:''
+    }
+  };
   users: User[];
-  showExtended : boolean = true;
-  loaded : boolean = false;
-  enabledAdd : boolean = true;
-  currentClasses = {};
-  currentStyles = {};
+  showExtended: boolean = true;
+  loaded: boolean = false;
+  enabledAdd: boolean = false;
+  showUserForm : boolean = false;
+
 
   constructor() { }
 
@@ -31,10 +40,9 @@ export class UsersComponent implements OnInit {
             city: 'Boston',
             state: 'MA'
           },
-          image: "http://lorempixel.com/600/600/people/3",
-          isActive : true,
-          balance : 100,
-          registered : new Date('01/02/2020 08:58:05')
+          isActive: true,
+          registered: new Date('01/02/2020 08:58:05'),
+          hide : true
         },
         {
           firstName: 'Kevin',
@@ -45,10 +53,9 @@ export class UsersComponent implements OnInit {
             city: 'Lynn',
             state: 'MA'
           },
-          image: "http://lorempixel.com/600/600/people/2",
           isActive: true,
-          balance : 500,
-          registered : new Date('09/11/2020 11:58:05')
+          registered: new Date('09/11/2020 11:58:05'),
+          hide : true
         },
         {
           firstName: 'Karen',
@@ -59,40 +66,49 @@ export class UsersComponent implements OnInit {
             city: 'Seani',
             state: 'Florida'
           },
-          image: "http://lorempixel.com/600/600/people/1",
-          isActive : true,
-          balance : 200,
-          registered : new Date('17/06/2020 05:58:05')
+          isActive: true,
+          registered: new Date('12/06/2020 05:58:05'),
+          hide : true
         }
       ];
 
       this.loaded = true;
-  
+
     }, 2000);
 
-    this.setCurrentClass();
-    this.setCurrentStyles();
-
     // this.showExtended = false;
+
+  }
+
+  addUser() {
+    this.user.isActive = true;
+    this.user.registered = new Date();
+    this.users.unshift(this.user);
+
+    this.user = {
+      firstName : '',
+      lastName : '',
+      age : null,
+      address : {
+        street : '',
+        city : '',
+        state:''
+      }
+    };
+  }
+
+  onSubmit(e){
+    console.log("12312");
+    e.preventDefault();
+  }
+
+  fireEvent(e){
+    console.log(e.type);
+    console.log(e.target.value);
     
   }
-
-  setCurrentClass(){
-    this.currentClasses = {
-      'btn-success' : this.enabledAdd,
-      'big-text' : this.showExtended
-    }
-  }
-
-  setCurrentStyles(){
-    this.currentStyles = {
-      'padding-top' : this.showExtended ? '0' : '40px',
-      'font-size' : this.showExtended ? '' : '40px'
-    }
-  }
-
-  addUser(user : User){
-    this.users.push(user);
-  }
+  // toggleHide(user: User){
+  //   user.hide = !user.hide;
+  // }
 
 }
